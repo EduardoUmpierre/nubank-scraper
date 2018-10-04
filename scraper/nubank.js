2e542c6871920bd0e94da42dedf14a26f8c51590
Eduardo Pieretti Umpierre <eduardoumpierre@hotmail.com>
2018-10-04 18:07:51 -0300

    Nubank navigation and scraper added

---
+++ scraper/nubank.js
@@ -0,0 +1,97 @@
+var casper = require('casper').create();
+casper.options.waitTimeout = 50000;
+
+var data = {};
+
+// Starts a connection to the follow URL
+casper.start('https://app.nubank.com.br/');
+
+// When the selector exists, fill it with this data and submit it
+casper.waitForSelector('form input[type=text]', function () {
+    var options = casper.cli.options;
+
+    this.sendKeys('input[type=text]', options['user']);
+    this.sendKeys('input[type=password]', options['password']);
+    this.click('button[type=submit]');
+});
+
+// Wait the form being destroyed
+casper.waitWhileSelector('form');
+
+// Gets the name and limits data
+casper.waitForSelector('.limitbar .portion', function () {
+    data.name = this.getElementInfo('.profile-title').text;
+    data.limits = getLimits();
+});
+
+// Gets the transactions data
+casper.waitForSelector('#feedTable > tbody .event-card.transaction', function () {
+    data.lastTransactions = getLastTransactions();
+
+    // Prints the data on console
+    this.echo(JSON.stringify(data));
+
+    // Closes the application
+    this.exit();
+});
+
+// Runs the casper methods and then print the collected data
+casper.run();
+
+/**
+ * Fetch the current page and get all limits values
+ */
+function getLimits() {
+    var response = {};
+
+    // Gets an array of available limits
+    var limits = casper.evaluate(function () {
+        var elements = document.querySelectorAll('.limitbar .portion');
+
+        return [].map.call(elements, function (element) {
+            var label = element.children[0].children[0].innerText.split('\n');
+            var response = {};
+
+            response[element.classList[1]] = {
+                text: label[0].trim(),
+                value: label[1].trim()
+            };
+
+            return response;
+        });
+    });
+
+    // Converts the arrays of objects in a single object
+    limits.forEach(function (item) {
+        for (var attr in item) {
+            response[attr] = item[attr];
+        }
+    });
+
+    return response;
+}
+
+/**
+ * Gets the last transactions
+ */
+function getLastTransactions() {
+    var transactions = casper.evaluate(function () {
+        var transactions = document.querySelectorAll('#feedTable > tbody .event-card.transaction');
+
+        return [].map.call(transactions, function (transaction) {
+            var data = transaction.children;
+            var response = {};
+
+            response = {
+                category: data[1].innerText,
+                title: data[2].innerText,
+                amount: data[3].innerText,
+                date: data[5].innerText
+            };
+
+            return response;
+        })
+    });
+
+    return transactions;
+}
