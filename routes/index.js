68b2f5b11950e2c098c5ea3e8e91408b51dfc6d1
Eduardo Pieretti Umpierre <eduardoumpierre@hotmail.com>
2018-10-04 18:03:50 -0300

    Home page and API post endpoint added

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var casper_wrapper = require('../casper_wrapper');
+var router = express.Router();
+
+/**
+ * @GET Home page
+ */
+router.get('/', function (req, res) {
+    res.render('index');
+});
+
+/**
+ * @POST Get data from Nubank
+ */
+router.post('/data', function (req, res) {
+    casper_wrapper(req.body.user, req.body.password).then(function (data) {
+        res.json(data);
+    });
+});
+
+module.exports = router;
