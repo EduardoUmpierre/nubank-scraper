9571e0364e5c55293e1a23a225ff89f964a7c740
Eduardo Pieretti Umpierre <eduardoumpierre@hotmail.com>
2018-10-05 11:45:25 -0300

    Quotes added to command

--- casper_wrapper.js
+++ casper_wrapper.js
@@ -3,8 +3,10 @@ var cmd = require('node-cmd');
 module.exports = function (user, password) {
     // Returns the promise
     return new Promise(function (resolve, reject) {
+        var command = 'casperjs ./scraper/nubank.js --user=\'' + user + '\' --password=\'' + password + '\'';
+
         // Runs the command and resolve the result
-        cmd.get('casperjs ./scraper/test.js --user=' + user + ' --password=' + password, function (err, data, stderr) {
+        cmd.get(command, function (err, data, stderr) {
             if (err) {
                 reject(err);
                 return;
